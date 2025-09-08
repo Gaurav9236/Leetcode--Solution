c803a3ff5d848a763a37c5cea6900031272bfff3
Gaurav9236 <enggaurav.eg@gmail.com>
2025-09-08 23:04:31 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.cpp
@@ -0,0 +1,18 @@
+// Last updated: 08/09/2025, 23:04:30
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int currSum = 0, maxSum = INT_MIN;
+
+        for(int val : nums){
+            currSum+=val;
+            maxSum = max(currSum,maxSum);
+            if(currSum < 0){
+                currSum =0 ;
+            }
+        }
+        return maxSum;
+    }
+     
+};
