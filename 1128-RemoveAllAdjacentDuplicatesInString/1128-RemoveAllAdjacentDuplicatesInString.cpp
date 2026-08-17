b81bd371faf41cd91ba9682932cc4ef827f5ca3f
Gaurav Yadav <enggaurav.eg@gmail.com>
2026-08-17 18:31:00 +0530

    Create 1128-RemoveAllAdjacentDuplicatesInString

---
+++ 1128-RemoveAllAdjacentDuplicatesInString/1128-RemoveAllAdjacentDuplicatesInString.cpp
@@ -0,0 +1,16 @@
+// Last updated: 17/08/2026, 18:30:59
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+        for (char ch : s){
+            if(!res.empty() && res.back() == ch)
+                res.pop_back();
+
+             else 
+               res.push_back(ch);   
+        }
+        return res;
+        
+    }
+};
