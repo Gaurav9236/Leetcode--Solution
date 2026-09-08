e8dc1043fd20172a067226de3c7fa4043765a16b
Gaurav Yadav <enggaurav.eg@gmail.com>
2026-09-08 07:55:26 +0530

    Create 3870-CountCommasinRange

---
+++ 3870-CountCommasinRange/3870-CountCommasinRange.cpp
@@ -0,0 +1,14 @@
+// Last updated: 08/09/2026, 07:55:26
+1class Solution {
+2public:
+3    int countCommas(int n) {
+4        int ans;
+5        if(n>=1000){
+6            ans = n-999;
+7             return ans;
+8        }
+9        return 0;
+10       
+11        
+12    }
+13};
